4209916697c74f196be6834edfa01a25da597444
Psionik K <73710933+psionic-k@users.noreply.github.com>
2024-11-19 17:59:17 +0900

    Off-by-one bug in markup hiding code
    This was causing images with associated keywords to be hidden unless they were
    preceded by a space, as seen in the demo.
    
    It is unknown what forces of evil compelled me to include the extraneous space
    before images in the demo.  Likely I encountered the issue while recording and
    made it work, intending to double back and fix it before not understanding that
    there was an issue when looking at my todo list.  Evil thrives in entropy and
    can succeed at its nefarious goals even when it is miniscule and even while good
    appears overwhelming.

--- dslide.el
+++ dslide.el
@@ -1218,7 +1218,7 @@ for `dslide-contents-map'.")
         (goto-char (oref obj begin))
         (while (re-search-forward org-keyword-regexp bound t)
           (let ((overlay (make-overlay (match-beginning 0)
-                                       (1+ (match-end 0)))))
+                                       (match-end 0))))
             (overlay-put overlay 'invisible t)
             (push dslide-overlays overlay)))))))
 
